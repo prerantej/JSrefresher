57b47c66809d18e38628fc37b253968796a77b26
prerantej <prerantejkvr@gmail.com>
2024-06-26 23:57:33 +0530

    dom continuation

--- Dom/index.js
+++ Dom/index.js
@@ -1,2 +1,26 @@
 // document.querySelector("h1").innerHTML="Good bye";
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Preran";
+
+
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Preran";
+
+
+// document.getElementById();
+// document.getElementsByClassName();
+// document.getElementsByTagName();
+// these all return a array so changing a property would not be a option insted what we should is specify the index
+
+// document.querySelector("here a class or id name cnan be given");
+// for Eg 
+// document.querySelector("h1").innerHTML="preran"; this will change the text to my name 
+
+// we cane use hierarchial method for select something
+// for Eg
+// document.querySelector("li a");
+// document.querySelector(".list a");
+// etc
+
+// document.querySelector("button").style.backgroundColor="yellow";
+
+// classlist would return all the classes of a element ina form of list you add delete class to it so that you can change style without actual actually specifing all the properties but instead we create a class where all the properties are defined 
+
+// document.querySelector("h1").classList.add("huge");
